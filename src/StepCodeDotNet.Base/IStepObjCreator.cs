8b0d9e8a94be8ded09e289869a9a71240e24be6c
thrace <xqy1739265310@163.com>
2025-07-12 18:49:44 +0800

    xxxx

--- src/StepCodeDotNet.Base/IStepObjCreator.cs
+++ src/StepCodeDotNet.Base/IStepObjCreator.cs
@@ -1,6 +1,292 @@
 namespace StepCodeDotNet.Base;
+
 using System.Collections.Frozen;
+using System.Text;
+
 public interface IStepObjCreator
 {
-    IStepObj[] CreateStepObjs(List<LineExpress> lineExpresses);
+    public Encoding Encoding { get; }
+
+    public IStepBaseObj Create(ReadOnlySpan<byte> entityName, IStepToken express);
+    public IStepBaseObj Create(string entityName, IStepToken express);
+    public IStepBaseObj Create(EntityToken express);
+
+    public Array CreateArray(string typeName, int size);
+
+    public IStepObj CreateComplex(string complexName);
+
+    public void InitStepObj(IStepObj obj, Dictionary<int, IStepObj> refMap);
+
+    public T GetEnum<T>(IStepToken express) where T : struct, Enum
+    {
+        if (express.TokenType != StepTokenType.Enum)
+        {
+            return (T)(object)-1;
+        }
+        var byteValue = express.GetEntityName();
+        Span<char> charValue = stackalloc char[byteValue.Length];
+        for (int i = 0; i < byteValue.Length; i++)
+        {
+            charValue[i] = (char)byteValue[i];
+        }
+        return Enum.Parse<T>(charValue);
+    }
+
+
+    public double GetREAL(IStepToken express)
+    {
+        return express.TokenType switch
+        {
+            StepTokenType.Real => express.GetRealValue(),
+            StepTokenType.Integer => express.GetIntegerValue(),
+            _ => 0.0,
+        };
+    }
+
+
+    public int GetINTEGER(IStepToken express)
+    {
+        return express.TokenType switch
+        {
+            StepTokenType.Integer => express.GetIntegerValue(),
+            _ => 0,
+        };
+    }
+
+    public double GetNUMBER(IStepToken express)
+    {
+        return express.TokenType switch
+        {
+            StepTokenType.Real => express.GetRealValue(),
+            StepTokenType.Integer => express.GetIntegerValue(),
+            _ => 0.0,
+        };
+    }
+
+    public byte[] GetBINARY(IStepToken express)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public string GetSTRING(IStepToken express)
+    {
+        return express.TokenType switch
+        {
+            StepTokenType.String => Encoding.GetString(express.GetEntityName()),
+            _ => string.Empty,
+        };
+    }
+
+
+    public bool GetBOOLEAN(IStepToken express) => express.TokenType switch
+    {
+        StepTokenType.Boolean => express.GetBooleanValue(),
+        _ => true,
+    };
+
+    public T GetEntity<T>(IStepToken express, Dictionary<int, IStepObj> refMap) where T : class
+    {
+        if (express.TokenType == StepTokenType.Entity)
+        {
+            var r = Create(express.GetEntityName(), express);
+            if (r is IStepObj stepObj)
+            {
+                InitStepObj(stepObj, refMap);
+            }
+            return r as T;
+        }
+        else if (express.TokenType == StepTokenType.LineNumber)
+        {
+            if (refMap.TryGetValue(express.GetLineNumber(), out var stepObj))
+            {
+                return stepObj as T;
+            }
+        }
+        return default;
+    }
+
+    public T GetBaseEntity<T>(IStepToken express, Dictionary<int, IStepObj> refMap) where T : unmanaged, IStepBaseObj => express.TokenType switch
+    {
+        StepTokenType.Entity => (T)Create(express.GetEntityName(), express),
+        _ => default,
+    };
+
+    private static T[] GetRefAggregate<T>(ReadOnlySpan<IStepToken> express, Dictionary<int, IStepObj> refMap)
+    {
+        var n = (express.Length + 1) / 2;
+        var r = new T[n];
+        int i = 0;
+        foreach (var token in express)
+        {
+            if (token.TokenType != StepTokenType.LineNumber)
+            {
+                continue;
+            }
+            if (refMap.TryGetValue(token.GetLineNumber(), out var stepObj))
+            {
+                r[i] = (T)stepObj;
+                i++;
+            }
+        }
+        if (i < n)
+        {
+            Array.Resize(ref r, i);
+        }
+        return r;
+    }
+
+    private Array GetRefAggregateObjs(ReadOnlySpan<IStepToken> express, Dictionary<int, IStepObj> refMap, string elementTypeName)
+    {
+        var n = (express.Length + 1) / 2;
+        var temp = new IStepObj[n];
+        int i = 0;
+        foreach (var token in express)
+        {
+            if (token.TokenType != StepTokenType.LineNumber)
+            {
+                continue;
+            }
+            if (refMap.TryGetValue(token.GetLineNumber(), out var stepObj))
+            {
+                temp[i] = stepObj;
+                i++;
+            }
+        }
+        var r = CreateArray(elementTypeName, i);
+        Array.Copy(temp, r, i);
+        return r;
+    }
+    private static Type CreateArrayType(Type elementType, int depth)
+    {
+        var type = elementType;
+        for (int i = 0; i < depth; i++)
+        {
+            type = type.MakeArrayType();
+        }
+        return type;
+    }
+
+    private Array GetList(ReadOnlySpan<IStepToken> express, Dictionary<int, IStepObj> refMap, int arrayDepth, Type elementType)
+    {
+        var bracketCount = arrayDepth - 1;
+        if (bracketCount == 0)
+        {
+            var typeName = elementType.Name.ToUpper();
+            var firstElement = express[0];
+            if (firstElement.TokenType == StepTokenType.LineNumber)
+            {
+                return GetRefAggregateObjs(express, refMap, typeName);
+            }
+            var n = (express.Length + 1) / 2;
+            var result = CreateArray(typeName, n);
+            int i = 0;
+            foreach (var token in express)
+            {
+                if (token.TokenType != firstElement.TokenType)
+                {
+                    continue;
+                }
+                var r = Create(typeName, token);
+                if (r is IStepObj stepObj)
+                {
+                    InitStepObj(stepObj, refMap);
+                }
+                result.SetValue(r, i);
+                i++;
+            }
+            return result;
+        }
+        var subArrays = new List<Array>();
+        var subTokensStart = 0;
+        int bracketLevel = 0;
+        for (int i = 0; i < express.Length; i++)
+        {
+            var token = express[i];
+            if (token.TokenType == StepTokenType.LeftBracket)
+            {
+                bracketLevel++;
+                if (bracketLevel == 1)
+                {
+                    subTokensStart = i + 1;
+                    continue;
+                }
+            }
+            else if (token.TokenType == StepTokenType.RightBracket)
+            {
+                bracketLevel--;
+                if (bracketLevel == 0)
+                {
+                    var subTokensLength = i - subTokensStart + 1;
+                    var subTokens = express.Slice(subTokensStart, subTokensLength);
+                    var subArray = GetList(subTokens, refMap, arrayDepth - 1, elementType);
+                    subArrays.Add(subArray);
+                    continue;
+                }
+            }
+        }
+
+        var arrayType = CreateArrayType(elementType, arrayDepth - 1);
+        var mdArray = Array.CreateInstance(arrayType, subArrays.Count);
+
+        for (int i = 0; i < subArrays.Count; i++)
+        {
+            mdArray.SetValue(subArrays[i], i);
+        }
+        return mdArray;
+    }
+
+    private static (int, Type) GetArrayDepthAndElementType(Type type)
+    {
+        int depth = 0;
+        while (type.IsArray)
+        {
+            depth++;
+            type = type.GetElementType();
+        }
+        return (depth, type);
+    }
+
+    public T[] GetAggregate<T>(ReadOnlySpan<IStepToken> express, Dictionary<int, IStepObj> refMap)
+    {
+
+        if (express.Length == 0)
+        {
+            return [];
+        }
+        var firstElement = express[0];
+        if (typeof(T).IsArray)
+        {
+            var (depth, elementType) = GetArrayDepthAndElementType(typeof(T[]));
+            return (T[])GetList(express, refMap, depth, elementType);
+        }
+
+        if (firstElement.TokenType == StepTokenType.LineNumber)
+        {
+            return GetRefAggregate<T>(express, refMap);
+        }
+        var typeName = typeof(T).Name.ToUpper();
+        var n = (express.Length + 1) / 2;
+        var result = new T[n];
+        int i = 0;
+        foreach (var token in express)
+        {
+            if (token.TokenType != firstElement.TokenType)
+            {
+                continue;
+            }
+            var r = Create(typeName, token);
+            if (r is IStepObj stepObj)
+            {
+                InitStepObj(stepObj, refMap);
+            }
+            result[i] = (T)r;
+            i++;
+        }
+        return result;
+    }
+
+
+    public IStepObj CreateComplex(ReadOnlySpan<IStepToken> complexExpress);
+
 }
--- src/StepCodeDotNet.Base/StepParser.cs
+++ src/StepCodeDotNet.Base/StepParser.cs
@@ -1,6 +1,7 @@
 ﻿namespace StepCodeDotNet.Base;
 
 using System.Collections.Concurrent;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -34,27 +35,29 @@ public unsafe partial class StepParser(IStepObjCreator creater)
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
         _gb18030 = Encoding.GetEncoding("GB18030");
     }
+
+    private List<IStepObj> _stepObjs = [];
+    Dictionary<int, IStepObj> _refMap = [];
+
+    public IStepObj[] GetStepObjs() => [.. _stepObjs];
+
+    public bool TryGetStepObj(int lineNumber, out IStepObj stepObj)
+    {
+        return _refMap.TryGetValue(lineNumber, out stepObj);
+    }
+
+    public IStepObj GetStepObj(int lineNumber)
+    {
+        return _refMap[lineNumber];
+    }
+
     public static long GetMemoryUsedMB()
     {
         var process = Process.GetCurrentProcess();
         return process.WorkingSet64 / (1024 * 1024);
     }
 
-    public IStepObj[] Resolve(string stepPath)
-    {
-        var totalWatch = Stopwatch.StartNew();
-        var lineExps = ResolveToExpress(stepPath);
-        Console.WriteLine($"After resolve express, Memory used: {GetMemoryUsedMB()} MB");
-        var stopWatch = Stopwatch.StartNew();
-        var stepObjs = creater.CreateStepObjs(lineExps);
-        stopWatch.Stop();
-        totalWatch.Stop();
-        Console.WriteLine($"Object creation took: {stopWatch.ElapsedMilliseconds} ms");
-        Console.WriteLine($"Total parsing took: {totalWatch.ElapsedMilliseconds} ms");
-        return stepObjs;
-    }
-
-    private List<LineExpress> ResolveToExpress(string stepPath)
+    public void Resolve(string stepPath)
     {
         var stopWatch = Stopwatch.StartNew();
         using var tokenizer = new StepTokenizer(stepPath);
@@ -64,202 +67,44 @@ public unsafe partial class StepParser(IStepObjCreator creater)
 #if DEBUG
         // PrintTokens(tokenLists);
 #endif
-        stopWatch.Restart();
-        var expressList = new List<LineExpress>();
+        CreateStepObjs(tokenizeResult);
+        return;
+    }
+
+    private void CreateStepObjs(StepTokenizeResult tokenizeResult)
+    {
+        var stopWatch = Stopwatch.StartNew();
+        this._stepObjs = new(tokenizeResult.Lines.Count);
+        this._refMap = new(tokenizeResult.Lines.Count);
         foreach (var line in tokenizeResult)
         {
-            var lineExpress = ResolveLine(line);
-            expressList.Add(lineExpress);
+            if (line.Length < 4)
+            {
+                continue; // Skip empty lines or lines with insufficient tokens
+            }
+            var lineNumber = line[1].GetLineNumber();
+            var lineBody = line[3..line.Length];
+            var thirdToken = line[3];
+            IStepBaseObj obj = null;
+            switch (thirdToken.TokenType)
+            {
+                case StepTokenType.Entity:
+                    obj = creater.Create(thirdToken.GetEntityName(), thirdToken);
+                    break;
+                case StepTokenType.LeftBracket:
+                    obj = creater.CreateComplex(lineBody);
+                    break;
+                default:
+                    break;
+            }
+            if (obj is IStepObj stepObj)
+            {
+                _stepObjs.Add(stepObj);
+                _refMap[lineNumber] = stepObj;
+            }
         }
         stopWatch.Stop();
         Console.WriteLine($"Expression resolution took: {stopWatch.ElapsedMilliseconds} ms");
-        return expressList;
-    }
-
-    private static (ListExpress, int) ResolveList(ReadOnlySpan<IStepToken> listTokens)
-    {
-        var result = new List<IExpress>();
-        for (int i = 0; i < listTokens.Length; i++)
-        {
-            var token = listTokens[i];
-            switch (token.TokenType)
-            {
-                case StepTokenType.Integer:
-                    result.Add(new IntegerExpress(token.GetIntegerValue()));
-                    break;
-                case StepTokenType.Real:
-                    result.Add(new RealExpress(token.GetRealValue()));
-                    break;
-                case StepTokenType.String:
-                    result.Add(new StringExpress(_gb18030.GetString(token.GetStringValue())));
-                    break;
-                case StepTokenType.Enum:
-                    result.Add(new EnumExpress(Encoding.ASCII.GetString(token.GetEnumValue())));
-                    break;
-                case StepTokenType.Boolean:
-                    result.Add(new BooleanExpress(token.GetBooleanValue()));
-                    break;
-                case StepTokenType.Asterisk:
-                    result.Add(new AsteriskExpress());
-                    break;
-                case StepTokenType.LineNumber:
-                    result.Add(new RefExpress(token.GetLineNumber()));
-                    break;
-                case StepTokenType.Dollar:
-                    result.Add(new DollarExpress());
-                    break;
-                case StepTokenType.Entity:
-                    {
-                        var (entityExpress, endIndex) = ResolveEntity(listTokens[i..]);
-                        result.Add(entityExpress);
-                        i += endIndex;
-                        break;
-                    }
-                case StepTokenType.LeftBracket:
-                    {
-                        var startIndex = i + 1;
-                        var (listExpress, endIndex) = ResolveList(listTokens[startIndex..]);
-                        result.Add(listExpress);
-                        i += endIndex + 1;
-                        break;
-                    }
-                case StepTokenType.RightBracket:
-                    return (new ListExpress(result), i);
-                default:
-                    break;
-            }
-        }
-        return (new ListExpress(result), 0);
-    }
-
-    private static ComplexExpress ResolveComplex(ReadOnlySpan<IStepToken> listTokens)
-    {
-        var result = new List<EntityExpress>();
-        for (int i = 0; i < listTokens.Length; i++)
-        {
-            var token = listTokens[i];
-            switch (token.TokenType)
-            {
-                case StepTokenType.Entity:
-                    {
-                        var (entityExpress, endIndex) = ResolveEntity(listTokens[i..]);
-                        result.Add(entityExpress);
-                        i += endIndex;
-                        break;
-                    }
-                case StepTokenType.LeftBracket:
-                    {
-                        var startIndex = i + 1;
-                        var (listExpress, endIndex) = ResolveList(listTokens[startIndex..]);
-                        result.AddRange(listExpress.ExpressList.Select(x => (EntityExpress)x));
-                        i += endIndex + 1;
-                        break;
-                    }
-                case StepTokenType.RightBracket:
-                    return new ComplexExpress(result);
-                default:
-                    break;
-            }
-        }
-        return new ComplexExpress(result);
-    }
-
-
-    private static (EntityExpress, int) ResolveEntity(ReadOnlySpan<IStepToken> entityTokens)
-    {
-        if (entityTokens.Length < 3)
-        {
-            throw new Exception("Invalid entity");
-        }
-        if (entityTokens[0].TokenType != StepTokenType.Entity)
-        {
-            throw new Exception("Invalid entity");
-        }
-        if (entityTokens[1].TokenType != StepTokenType.LeftBracket)
-        {
-            throw new Exception("Invalid entity");
-        }
-        var entityName = Encoding.ASCII.GetString(entityTokens[0].GetEntityName());
-        var args = new List<IExpress>();
-        for (int i = 2; i < entityTokens.Length; i++)
-        {
-            var token = entityTokens[i];
-            switch (token.TokenType)
-            {
-                case StepTokenType.Integer:
-                    args.Add(new IntegerExpress(token.GetIntegerValue()));
-                    break;
-                case StepTokenType.Real:
-                    args.Add(new RealExpress(token.GetRealValue()));
-                    break;
-                case StepTokenType.String:
-                    args.Add(new StringExpress(_gb18030.GetString(token.GetStringValue())));
-                    break;
-                case StepTokenType.Enum:
-                    args.Add(new EnumExpress(Encoding.ASCII.GetString(token.GetEnumValue())));
-                    break;
-                case StepTokenType.Boolean:
-                    args.Add(new BooleanExpress(token.GetBooleanValue()));
-                    break;
-                case StepTokenType.Asterisk:
-                    args.Add(new AsteriskExpress());
-                    break;
-                case StepTokenType.Dollar:
-                    args.Add(new DollarExpress());
-                    break;
-                case StepTokenType.LineNumber:
-                    args.Add(new RefExpress(token.GetLineNumber()));
-                    break;
-                case StepTokenType.Entity:
-                    {
-                        var (entityExpress, endIndex) = ResolveEntity(entityTokens[i..]);
-                        args.Add(entityExpress);
-                        i += endIndex;
-                        break;
-                    }
-                case StepTokenType.LeftBracket:
-                    {
-                        var startIndex = i + 1;
-                        var (listExpress, endIndex) = ResolveList(entityTokens[startIndex..]);
-                        args.Add(listExpress);
-                        i += endIndex + 1;
-                        break;
-                    }
-                case StepTokenType.RightBracket:
-                    return (new EntityExpress(entityName, args), i);
-                default:
-                    break;
-            }
-        }
-        return (new EntityExpress(entityName, args), 0);
-    }
-
-    private static LineExpress ResolveLine(ReadOnlySpan<IStepToken> lineTokens)
-    {
-        if (lineTokens.Length < 3)
-        {
-            throw new Exception("Invalid line");
-        }
-        if (lineTokens[0].TokenType != StepTokenType.LineNumber)
-        {
-            throw new Exception("Invalid line number");
-        }
-        var lineNumberValue = lineTokens[0].GetLineNumber();
-        if (lineTokens[1].TokenType != StepTokenType.Equal)
-        {
-            throw new Exception("Invalid equal sign");
-        }
-        switch (lineTokens[2].TokenType)
-        {
-            case StepTokenType.Entity:
-                var (entityExpress, _) = ResolveEntity(lineTokens[2..]);
-                return new LineExpress(lineNumberValue, entityExpress);
-            case StepTokenType.LeftBracket:
-                var complexExpress = ResolveComplex(lineTokens[3..]);
-                return new LineExpress(lineNumberValue, complexExpress);
-            default:
-                throw new Exception("Invalid entity");
-        }
     }
 
     private static void PrintTokens(StepTokenizeResult tokens)
@@ -327,82 +172,5 @@ public unsafe partial class StepParser(IStepObjCreator creater)
     }
 
 
-
-
-    // private static ConcurrentBag<MList<IStepToken>> Tokenize(string stepFile)
-    // {
-    //     using var fileStream = new FileStream(stepFile, FileMode.Open, FileAccess.Read);
-    //     using var reader = new BinaryReader(fileStream);
-    //     SkipHeader(fileStream);
-
-
-    //     var tokens = new ConcurrentBag<MList<IStepToken>>();
-    //     var channel = Channel.CreateUnbounded<string[]>();
-    //     const int batchSize = 100;
-
-    //     var producer = Task.Run(() =>
-    //     {
-    //         var producerWatch = Stopwatch.StartNew();
-    //         var batch = new string[batchSize];
-    //         int batchIndex = 0;
-    //         using UMList<byte> sb = new(2048);
-    //         Span<byte> buffer = stackalloc byte[1];
-    //         while (reader.Read(buffer) > 0)
-    //         {
-    //             if (buffer[0] == '\r')
-    //             {
-    //                 continue;
-    //             }
-    //             if (buffer[0] == '\n')
-    //             {
-    //                 continue;
-    //             }
-    //             sb.Add(buffer[0]);
-    //             if (buffer[0] == ';')
-    //             {
-    //                 var line = _gb18030.GetString(sb.AsReadOnlySpan());
-    //                 sb.Clear();
-    //                 if (line.StartsWith("ENDSEC;"))
-    //                 {
-    //                     break;
-    //                 }
-    //                 batch[batchIndex++] = line;
-    //                 if (batchIndex == batchSize)
-    //                 {
-    //                     channel.Writer.WriteAsync(batch);
-    //                     batch = new string[batchSize];
-    //                     batchIndex = 0;
-    //                 }
-    //             }
-    //         }
-    //         if (batchIndex > 0)
-    //         {
-    //             Array.Resize(ref batch, batchIndex);
-    //             channel.Writer.TryWrite(batch);
-    //         }
-    //         channel.Writer.Complete();
-    //         producerWatch.Stop();
-    //         Console.WriteLine($"Producer completed in {producerWatch.ElapsedMilliseconds} ms");
-    //     });
-    //     var consumerWatch = Stopwatch.StartNew();
-    //     int workerCount = Environment.ProcessorCount;
-    //     var consumers = Enumerable.Range(0, workerCount).Select(_ => Task.Run(async () =>
-    //     {
-    //         await foreach (var batch in channel.Reader.ReadAllAsync())
-    //         {
-    //             foreach (var line in batch)
-    //             {
-    //                 var lineTokens = TokenizeLine(line);
-    //                 tokens.Add(lineTokens);
-    //             }
-    //         }
-    //     })).ToArray();
-    //     Task.WaitAll(consumers);
-    //     consumerWatch.Stop();
-    //     Console.WriteLine($"Consumer completed in {consumerWatch.ElapsedMilliseconds} ms");
-    //     return tokens;
-    // }
-
-    [GeneratedRegex(@"^[-+]?[0-9]+(\.[0-9]*)?([eE][-+]?[0-9]+)?", RegexOptions.Compiled | RegexOptions.CultureInvariant)]
-    private static partial Regex NumberRegex();
 }
+
